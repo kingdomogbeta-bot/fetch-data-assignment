7d1dfb9a8034e15c00dd8863dd177399e1caa8d5
kingdom <kingdomogbeta@gmail.com>
2025-10-31 10:18:18 +0100

    commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function fetchUsersAndSummarize() {
+async function fetchUsersAndSummarize() {
   const userUrl = "https://jsonplaceholder.typicode.com/users";
   fetch(userUrl)
     .then((response) => {
       if (!response.ok) {
-        throw new Error(Request failed, Status code: ${response.status});
+        throw new Error(`Request failed, Status code: ${response.status}`);
       }
       console.log(response);
       return response.json();
@@ -17,33 +17,28 @@ function fetchUsersAndSummarize() {
           companyName: user.company.name,
         }))
         .forEach((user) =>
-          console.log(
-            User ID ${user.id}: ${user.name} works at ${user.companyName}
-          )
+          console.log(`User ID ${user.id}: ${user.name} works at ${user.companyName}`)
         ); 
     })
     .catch((error) => {
-      console.error(Error fetching or processing users: ${error.message});
+      console.error(`Error fetching or processing users: ${error.message}`);
     });
 }
 
-// Demonstrating successful fetch 
 fetchUsersAndSummarize();
 
-// Demonstrating Failure using an invalid endpoint
 function testError() {
   fetch("https://jsonplaceholder.typicode.com/u5ers")
     .then((response) => {
       if (!response.ok) {
-        throw new Error(Request failed! Status code: ${response.status});
+        throw new Error(`Request failed! Status code: ${response.status}`);
       }
       return response.json();
     })
     .then((data) => console.log(data))
     .catch((error) =>
-      console.error(TestError Catch Block: ${error.message})
+      console.error(`TestError Catch Block: ${error.message}`)
     );
 }
 
-// Check error handling
 testError();
